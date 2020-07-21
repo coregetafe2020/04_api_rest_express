26da291bae84ecbdad7fd39b1a1bcbdd7a277d0c
coredummie2020 <coredummie2020@gmail.com>
2020-07-21 10:48:46 +0200

    'put'

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 let proveedores = [
     {_id: 1, nombre: 'Gas Natural', cif: 'A123456', domicilio: 'Bilbao'},
@@ -8,9 +11,23 @@ let proveedores = [
     {_id: 3, nombre: 'Planeta', cif: 'A345126', domicilio: 'Barcelona'}
 ]
 
-app.get('/', (req, res) => {
-    res.status(200).json(proveedores);
+// app.get('/', (req, res) => {
+//     res.status(200).json(proveedores);
+// })
+
+
+
+// get con queries-string en la url
+
+app.get('/consulta', (req, res) => {
+    console.log(req.query);
+    let nombre = req.query.nombre;
+    let cif = req.query.cif;
+    // Código de consulta con los valores de nombre y cif
+    res.status(200).json({mensaje: 'Test con queries-string'});
 })
+
+// get con parámetros en la url
 
 app.get('/:p_id', (req, res) => {
     let proveedor = proveedores.find((prov) => {
@@ -21,6 +38,40 @@ app.get('/:p_id', (req, res) => {
     }
     res.status(200).json(proveedor);
 }) 
+
+// Post para crear registros
+
+app.post('/', (req, res) => {
+    let proveedor = req.body;
+    proveedor._id = proveedores.length + 1;
+    proveedores.push(proveedor);
+    res.status(200).json({
+        mensaje: 'El proveedor se ha creado correctamente',
+        proveedores: proveedores
+    });
+})
+
+// Put para actualizar registros
+
+app.put('/:_id', (req, res) => {
+    let posicion = proveedores.findIndex(proveedor => {
+        return proveedor._id == req.params._id;
+    })
+    if (req.body.nombre !== undefined) {
+        proveedores[posicion].nombre = req.body.nombre;
+    }
+    if (req.body.domicilio !== undefined) {
+        proveedores[posicion].domicilio = req.body.domicilio;
+    }
+    if (req.body.cif !== undefined) {
+        proveedores[posicion].cif = req.body.cif;
+    }
+    res.status(200).json({
+        mensaje: 'El proveedor se actualizó correctamente'
+    })
+})
+
+
 
 app.listen(3000, () => {
     console.log('Servidor escuchando en http://localhost:3000');
